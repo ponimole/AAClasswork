3ab0d181ecb89097e8fa39a84d456151ce34e8e7
ponimole <onimolep@yahoo.com>
2020-01-14 11:42:01 -0600

    Wrote PolyTreeNode class initialize method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,22 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=
+
+    end
+    
+    def add_child
+
+    end
+
+    def remove_child
+
+    end
 
 end
--- skeleton/polytreenode.rb
+++
@@ -1,9 +0,0 @@
-class PolyTreeNode
-
-    def initialize(value)
-        @value = value
-        @parent = nil
-        @children = []
-    end
-
-end
