4826c1c2e610e981c543a6f50b2e9e508ab4d827
ponimole <onimolep@yahoo.com>
2020-01-15 11:16:17 -0600

    Created tic_tac_to_node.rb

---
+++ Tic-tac-toe AI/lib/tic_tac_toe_node.rb
@@ -0,0 +1,83 @@
+require_relative 'tic_tac_toe'
+require "byebug"
+
+class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+  def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    
+    @prev_move_pos = prev_move_pos
+  end
+
+
+
+  def losing_node?(evaluator)
+    winner = @board.winner
+    
+    if @board.over?
+      if winner == alternate_mark(evaluator)
+        return true
+      elsif winner == nil || winner == evaluator
+        return false
+      end
+    end
+    
+    return true if children.all? do |child|
+      child.losing_node?(evaluator)
+    end
+
+    return true if children.any? do |child|
+      child.losing_node?(evaluator)
+    end
+    false
+  end
+
+  def winning_node?(evaluator)
+    winner = @board.winner
+    
+    if @board.over?
+      if winner == evaluator
+        return true
+      elsif winner == nil || winner == alternate_mark(evaluator)
+        return false
+      end
+    end
+    
+    return true if children.any? do |child|
+      child.winning_node?(evaluator)
+    end
+
+    return true if children.all? do |child|
+      child.winning_node?(evaluator)
+    end
+    false
+  end
+
+  # This method generates an array of all moves that can be made after
+  # the current move.
+  def children
+    game_states = []
+    
+    @board.rows.each_with_index do |row,row_i|
+      row.each_with_index do |pos,col_i|
+        if pos.nil?
+          new_board = @board.dup
+          new_board.rows[row_i][col_i] = @next_mover_mark
+          prev_move_pos = [row_i,col_i]
+          child = TicTacToeNode.new(new_board, alternate_mark(@next_mover_mark), prev_move_pos)
+          game_states << child
+        end
+      end
+    end
+    game_states
+  end
+
+  def alternate_mark(mark)
+    if mark == :x
+      :o 
+    elsif mark == :o
+      :x
+    end
+  end
+end
