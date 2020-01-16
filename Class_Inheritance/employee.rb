b2a15fa60347f2f8bc6aa161e0ddc30347c5ac64
ponimole <onimolep@yahoo.com>
2020-01-16 15:33:12 -0600

    Created Manager class

--- Class_Inheritance/employee.rb
+++ Class_Inheritance/employee.rb
@@ -1,10 +1,19 @@
 class Employee
     attr_reader :name, :title, :salary, :boss
 
-    def initialize(name, title, salary,:boss)
+    def initialize(name, title, salary, boss)
         @name = name
         @title = title
         @salary = salary
         @boss = boss
+    end
+end
+
+class Manager < Employee
+    attr_reader :employees
+
+    def initialize(name, title, salary, boss, employees)
+        super
+        @employees = employees
     end
 end
